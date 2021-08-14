a6447fef141e01ba8e209bd11debd92027bf9577
santafg <santanughosh28111998@gmail.com>
2021-08-14 19:38:19 +0530

    adding cloudinary

--- routers/uploadRouter.js
+++ routers/uploadRouter.js
@@ -1,20 +1,26 @@
 const router = require("express").Router();
 const multer = require("multer");
+const cloudinary = require("cloudinary");
 const { isAuth } = require("../utils");
+require("../handlers/cloudinary");
 
-const storage = multer.diskStorage({
-  destination(req, file, cb) {
-    cb(null, "uploads/");
-  },
-  filename(req, file, cb) {
-    cb(null, `${Date.now()}.jpg`);
-  },
-});
+// const storage = multer.diskStorage({
+//   destination(req, file, cb) {
+//     cb(null, "uploads/");
+//   },
+//   filename(req, file, cb) {
+//     cb(null, `${Date.now()}.jpg`);
+//   },
+// });
+
+const storage = multer.diskStorage({});
 
 const upload = multer({ storage });
 
-router.post("/", isAuth, upload.single("image"), (req, res) => {
-  res.send(`/${req.file.path}`);
+router.post("/", isAuth, upload.single("image"), async (req, res) => {
+  // res.send(`/${req.file.path}`);
+  const result = await cloudinary.v2.uploader.upload(req.file.path);
+  res.send(result.secure_url);
 });
 
 module.exports = router;
